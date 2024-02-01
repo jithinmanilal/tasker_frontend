54203a16ad572fe64cfed324a59ed352d88fcbf0
Jithin Manilal <jithinmanilal@gmail.com>
2024-02-01 19:20:27 +0530

    final push

--- src/context/Constants.jsx
+++ src/context/Constants.jsx
@@ -7,4 +7,6 @@
 // export {BASE_URL};
 
 // local machine
-export const BASE_URL = 'http://localhost:8000'
+// export const BASE_URL = 'http://localhost:8000'
+
+export const BASE_URL = 'http://54.196.172.76'
